5732b8877269a71997106b499907507e0455fd1d
sofeien <444023990@qq.com>
2016-08-17 11:32:08 +0800

    创建随机数

---
+++ 2.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+import random
+
+ROWS=0
+COLUMNS=1
+MIN=2
+MAX=3
+
+titles=['行数','列数','最小值','最大值']
+values=[]
+def inputValue(title):
+    while True:
+        value=input("请输入需要的"+title+":")
+        try:
+            value=int(value)
+            values.append(value)
+            break
+        except ValueError:
+            print('输入错误，请输入整数')
+            continue
+
+for i in titles:
+    inputValue(i)
+
+for i in range(values[ROWS]):
+    for j in range(values[COLUMNS]):
+        value=random.randint(values[MIN],values[MAX])
+        print("{:>5}".format(value),end=' ')
+    print() 
